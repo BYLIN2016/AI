bb2fc8bb8bd0884a5f38b15b9dd62191ac70f6a3
1685025511 <1685025511@qq.com>
2017-09-01 13:12:17 +0800

    000

---
+++ common.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mumu
+{
+    static class common
+    {
+        //移动鼠标 
+        public const int MOUSEEVENTF_MOVE = 0x0001;
+        //模拟鼠标左键按下 
+        public const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        //模拟鼠标左键抬起 
+        public const int MOUSEEVENTF_LEFTUP = 0x0004;
+        //模拟鼠标右键按下 
+        public const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        //模拟鼠标右键抬起 
+        public const int MOUSEEVENTF_RIGHTUP = 0x0010;
+        //模拟鼠标中键按下 
+        public const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+        //模拟鼠标中键抬起 
+        public const int MOUSEEVENTF_MIDDLEUP = 0x0040;
+        //标示是否采用绝对坐标 
+        public const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+
+        /*模拟键盘*/
+        public const int WM_KEYDOWN = 0x100;
+        public const int WM_KEYUP = 0X101;
+        /*模拟发送文本*/
+        public const int WM_SETTEXT = 0x0C;
+        public const int WM_CHAR = 0x102;
+
+
+        //设置激活窗体
+        [DllImport("user32.dll")]
+        public static extern void SwitchToThisWindow(IntPtr hWnd, bool fAltTab);
+        //寻找目标进程窗口       
+        [DllImport("user32.DLL")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        //寻找目标进程子窗口
+        [DllImport("user32.DLL", EntryPoint = "FindWindowEx", SetLastError = true)]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+        //设置进程窗口到最前       
+        [DllImport("user32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+        //模拟键盘事件         
+        [DllImport("user32.DLL")]
+        public static extern void keybd_event(Byte bVk, Byte bScan, Int32 dwFlags, Int32 dwExtraInfo);
+        //给CheckBox发送信息
+        [DllImport("user32.DLL", EntryPoint = "SendMessage", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int SendMessage(IntPtr hwnd, UInt32 wMsg, int wParam, int lParam);
+        //给Text发送信息
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        public static extern int SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, string lParam);
+        [DllImport("user32.DLL")]
+        public static extern IntPtr GetWindow(IntPtr hWnd, int wCmd);
+        [DllImport("user32.dll ", EntryPoint = "GetDlgItem")]
+        public static extern IntPtr GetDlgItem(IntPtr hParent, int nIDParentItem);
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="Msg"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", EntryPoint = "PostMessage")]
+        public static extern bool PostMessage(IntPtr hWnd, UInt32 Msg, int wParam, int lParam);
+        /// <summary>
+        /// 获取文本内容
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="lpString"></param>
+        /// <param name="nMaxCount"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetWindowText(HandleRef hWnd, StringBuilder lpString, int nMaxCount);
+        /// <summary>
+        /// 获取文本长度
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetWindowTextLength(HandleRef hWnd);
+        /// <summary>
+        /// 获取句柄或者窗体
+        /// </summary>
+        /// <param name="callback"></param>
+        /// <param name="extraData"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool EnumWindows(EnumThreadWindowsCallback callback, IntPtr extraData);
+        /// <summary>
+        /// 获取子窗体以及子控件
+        /// </summary>
+        /// <param name="hwndParent"></param>
+        /// <param name="lpEnumFunc"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", ExactSpelling = true)]
+        public static extern bool EnumChildWindows(HandleRef hwndParent, EnumChildrenCallback lpEnumFunc, HandleRef lParam);
+        public delegate bool EnumThreadWindowsCallback(IntPtr hWnd, IntPtr lParam);
+        public delegate bool EnumChildrenCallback(IntPtr hwnd, IntPtr lParam);
+        /// <summary>
+        /// 获取类名
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="lpClassName"></param>
+        /// <param name="nMaxCount"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+        /// <summary>
+        /// 发送文本
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="Msg"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        [DllImport("User32.dll", EntryPoint = "SendMessage", CharSet = CharSet.Auto)]
+        public static extern int SendTextMessage(IntPtr hWnd,int Msg,int wParam,string lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int Width, int Height, int flags);
+        [DllImport("user32.dll", EntryPoint = "SetCursorPos")]
+        public static extern int SetCursorPos(int x, int y);
+
+        [System.Runtime.InteropServices.DllImport("user32")]
+        public static extern int mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+
+        /*加载speech处理类*/
+
+        /*加载opencv处理类*/
+
+        /*加载caffe处理类*/
+
+        /*封装第三方api接口*/
+    }
+}
---
+++ 控制台.cs
@@ -0,0 +1,126 @@
+﻿using mumu;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Speech.Recognition;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 语音控制登录
+{
+    public partial class 控制台 : Form
+    {
+        //实例化语音识别引擎
+        private SpeechRecognitionEngine sre = new SpeechRecognitionEngine();
+        //全局变量
+        List<IntPtr> list = new List<IntPtr>();
+        public 控制台()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*初始化语音*/
+            sre.SetInputToDefaultAudioDevice();
+            GrammarBuilder GB = new GrammarBuilder();
+            /*设置语音样本*/
+            GB.Append(new Choices(new string[] { "开关" }));
+            Grammar G = new Grammar(GB); G.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(G_SpeechRecognized);
+            sre.LoadGrammar(G); sre.RecognizeAsync(RecognizeMode.Multiple);
+        }
+        void G_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (e.Result.Text.Equals("开关"))
+            {
+                /*获取应用程序名称*/
+                string pName = "GSFramework";
+                /*查找指定应用程序名称的进程*/
+                Process[] temp = Process.GetProcessesByName(pName);//在所有已启动的进程中查找需要的进程；  
+                                                                   /*获取父窗体句柄【外围大框】 WindowsForms10.Window.8.app.0.33ec00f_r9_ad1*/
+                                                                   //IntPtr wcHandle = common.FindWindow("WindowsForms10.Window.8.app.0.33ec00f_r9_ad1", null);
+                if (temp.Length > 0)//如果查找到  
+                {
+                    IntPtr handle = temp[0].MainWindowHandle;
+                    //激活,显示在最前 
+                    common.SwitchToThisWindow(handle, true);
+                    /*获取窗体以及子窗体*/
+                    common.EnumThreadWindowsCallback callback1 = new common.EnumThreadWindowsCallback(EnumWindowsCallback);
+                    common.EnumWindows(callback1, IntPtr.Zero);
+                    /*查找子窗体*/
+                    //common.EnumChildWindows(new HandleRef(this, handle), new common.EnumChildrenCallback(EnumChildWindowsCallback), new HandleRef(null, IntPtr.Zero));
+                    MessageBox.Show("ok");
+                    /*登录系统*/
+                    /*账号密码*/
+                    common.SendTextMessage(list[2], common.WM_SETTEXT, 0, "linwf");
+                    common.SendMessage(list[0], common.WM_CHAR, 'l', 0);
+                    common.SendMessage(list[0], common.WM_CHAR, 'i', 0);
+                    common.SendMessage(list[0], common.WM_CHAR, 'n', 0);
+                    common.SendMessage(list[0], common.WM_CHAR, '_', 0);
+                    common.SendMessage(list[0], common.WM_CHAR, '1', 0);
+                    common.SendMessage(list[0], common.WM_CHAR, '2', 0);
+                    common.SendMessage(list[0], common.WM_CHAR, '3', 0);
+
+                    common.SetCursorPos(0, 0); //设置鼠标位置发送点击命令
+                    common.mouse_event(common.MOUSEEVENTF_MOVE, 500, 280, 0, 0);
+                    common.mouse_event(common.MOUSEEVENTF_LEFTDOWN, 500, 280, 0, 0);
+                    common.mouse_event(common.MOUSEEVENTF_LEFTUP, 500, 280, 0, 0);
+
+                }
+                else
+                {
+
+                    Process.Start("C:\\Users\\linwf\\AppData\\Roaming\\Inspur\\GSPSetup\\" + pName + ".exe");
+                }
+            }
+
+
+        }
+        private bool EnumWindowsCallback(IntPtr handle, IntPtr extraParameter)
+        {
+            int num1 = common.GetWindowTextLength(new HandleRef(this, handle)) * 2;
+            StringBuilder builder1 = new StringBuilder(num1);
+            common.GetWindowText(new HandleRef(this, handle), builder1, builder1.Capacity);
+            System.Console.WriteLine(string.Format("Wnd:{0} Title: {1}", handle, builder1.ToString()));
+            Application.DoEvents();
+            common.EnumChildWindows(new HandleRef(this, handle), new common.EnumChildrenCallback(EnumChildWindowsCallback), new HandleRef(null, IntPtr.Zero));
+            return true;
+        }
+        private bool EnumChildWindowsCallback(IntPtr handle, IntPtr lparam)
+        {
+
+            StringBuilder s = new StringBuilder(2000);
+            int i = common.GetClassName(handle, s, 255);
+            if (s.ToString() == "WindowsForms10.EDIT.app.0.33ec00f_r9_ad1")
+            {
+                list.Add(handle);
+            }
+            return true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            /*登录客户端*/
+   
+        }
+
+        private void 控制台_Load(object sender, EventArgs e)
+        {
+
+            /*初始化语音*/
+            sre.SetInputToDefaultAudioDevice();
+            GrammarBuilder GB = new GrammarBuilder();
+            /*设置语音样本*/
+            GB.Append(new Choices(new string[] { "登录浪潮" }));
+            Grammar G = new Grammar(GB); G.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(G_SpeechRecognized);
+            sre.LoadGrammar(G); sre.RecognizeAsync(RecognizeMode.Multiple);
+            timer1.Interval = 1000;//毫秒为单位
+        }
+    }
+}
